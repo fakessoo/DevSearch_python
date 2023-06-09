8979828ad6b22ec78095a153dd96264395203892
fakessoo <wlghksdl@naver.com>
2023-06-08 21:51:04 -0700

    Signals

--- users/models.py
+++ users/models.py
@@ -22,7 +22,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self) -> str:
-        return str(self.user.username)
+        return str(self.username)
     
 class Skill(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
@@ -33,3 +33,4 @@ class Skill(models.Model):
 
     def __str__(self) -> str:
         return str(self.name)
+
